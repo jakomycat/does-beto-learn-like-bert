6d333d347c38d53f0fc6bb99973c92461957ea82
jakomycat <jchalarcav@gmail.com>
2026-04-11 19:22:18 -0500

    Change in how the model is brought in.

--- src/extractor.py
+++ src/extractor.py
@@ -1,17 +1,17 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModel, AutoTokenizer
 import torch
 
 # Function to get pre-trained model
 def load_model_and_tokenizer(lang, device):
     # To BERT
     if lang == 'en':
-        model_name = 'bert-base-cased'
+        model_name = 'bert-base-uncased'
         
     # To BETO
     elif lang == 'es':
         raise ValueError('Its not implemented')
     
-    model = AutoModelForCausalLM.from_pretrained(model_name, output_hidden_states=True)
+    model = AutoModel.from_pretrained(model_name, output_hidden_states=True)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     
     model.to(device)
