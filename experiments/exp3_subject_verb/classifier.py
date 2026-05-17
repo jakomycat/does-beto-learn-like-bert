3356e87163020ab891ade824bd75ddb20259e811
jakomycat <jchalarcav@gmail.com>
2026-05-17 09:55:09 -0500

    Load the data layer by layer, rather than all at once.

--- experiments/exp3_subject_verb/classifier.py
+++ experiments/exp3_subject_verb/classifier.py
@@ -4,6 +4,8 @@ from sklearn.metrics import accuracy_score
 
 from pathlib import Path
 import pandas as pd
+import h5py
+import numpy as np
 
 from experiments.exp2_probing_task.classifier import ProbeClassifier, create_dataloader, train_probe # To reuse code
 
@@ -27,23 +29,26 @@ def evaluate_by_buckets(model, X_test_layer, y_test, buckets, device):
             
     return bucket_results
 
-def run_full_sva_evaluation(X_train, y_train, X_val, y_val, X_test, y_test, buckets, num_classes, device, output_filename):
-    num_layers = X_train.shape[1]
+def run_full_sva_evaluation(X_train_path, y_train, X_val_path, y_val, X_test_path, y_test, buckets, num_classes, device, output_filename):
+    # It's only to know how many layers has the model
+    with h5py.File(X_train_path, 'r') as f:
+        _, num_layers, hidden_size = f['verb_outputs'].shape
+    
     all_layer_data = []
 
     for layer_idx in range(num_layers):
         print(f'Current layer: {layer_idx}')
         
-        # Get data of one layer
-        X_train_l = X_train[:, layer_idx, :]
-        X_val_l = X_val[:, layer_idx, :]
-        X_test_l = X_test[:, layer_idx, :]
+        with h5py.File(X_train_path, 'r') as f_train, h5py.File(X_val_path, 'r') as f_val, h5py.File(X_test_path, 'r') as f_test:
+            X_train_l = f_train['verb_outputs'][:, layer_idx, :].astype(np.float32)
+            X_val_l = f_val['verb_outputs'][:, layer_idx, :].astype(np.float32)
+            X_test_l = f_test['verb_outputs'][:, layer_idx, :].astype(np.float32)
         
         train_loader = create_dataloader(X_train_l, y_train, device, is_train=True)
         val_loader = create_dataloader(X_val_l, y_val, device, is_train=False)
         
         # Model train
-        model = ProbeClassifier(X_train_l.shape[1], num_classes).to(device)
+        model = ProbeClassifier(hidden_size, num_classes).to(device)
         model = train_probe(model, train_loader, val_loader)
         
         # Evaluation
@@ -52,6 +57,8 @@ def run_full_sva_evaluation(X_train, y_train, X_val, y_val, X_test, y_test, buck
         row_results.update(bucket_metrics)
         
         all_layer_data.append(row_results)
+        
+        del X_train_l, X_val_l, X_test_l, train_loader, val_loader, model # Free up GPU memory
 
     # Save data
     df_results = pd.DataFrame(all_layer_data)
