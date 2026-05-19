5a30227b083e5f9588e24dea0f8c004812ad12a1
jakomycat <jchalarcav@gmail.com>
2026-05-19 14:18:58 -0500

    Optimized for optimal data reading.

--- experiments/exp3_subject_verb/classifier.py
+++ experiments/exp3_subject_verb/classifier.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import pandas as pd
 import h5py
 import numpy as np
+import gc
 
 from experiments.exp2_probing_task.classifier import ProbeClassifier, create_dataloader, train_probe # To reuse code
 
@@ -40,26 +41,38 @@ def run_full_sva_evaluation(X_train_path, y_train, X_val_path, y_val, X_test_pat
     for layer_idx in range(num_layers):
         print(f'Current layer: {layer_idx}')
         
-        with h5py.File(X_train_path, 'r') as f_train, h5py.File(X_val_path, 'r') as f_val, h5py.File(X_test_path, 'r') as f_test:
+        # Read train file
+        with h5py.File(X_train_path, 'r') as f_train:
             X_train_l = f_train['verb_outputs'][:, layer_idx, :]
-            X_val_l = f_val['verb_outputs'][:, layer_idx, :]
-            X_test_l = f_test['verb_outputs'][:, layer_idx, :]
-        
         train_loader = create_dataloader(X_train_l, y_train, device, is_train=True)
-        val_loader = create_dataloader(X_val_l, y_val, device, is_train=False)
+        del X_train_l # Free up memory
+        
+        # Read validation file
+        with h5py.File(X_val_path, 'r') as f_val:
+            X_val_l = f_val['verb_outputs'][:, layer_idx, :]
+        val_loader = create_dataloader(X_val_l, y_val, device, is_train=False)    
+        del X_val_l # Free up memory        
         
         # Model train
         model = ProbeClassifier(hidden_size, num_classes).to(device)
         model = train_probe(model, train_loader, val_loader)
+        del train_loader, val_loader
         
         # Evaluation
-        row_results = {'layer': layer_idx}
-        bucket_metrics = evaluate_by_buckets(model, X_test_l, y_test, buckets, device)
-        row_results.update(bucket_metrics)
+        with h5py.File(X_test_path, 'r') as f_test:
+            X_test_l = f_test['verb_outputs'][:, layer_idx, :]
         
+        row_results = {'layer': layer_idx}
+        
+        with torch.no_grad():
+            bucket_metrics = evaluate_by_buckets(model, X_test_l, y_test, buckets, device)
+        
+        row_results.update(bucket_metrics)
         all_layer_data.append(row_results)
         
-        del X_train_l, X_val_l, X_test_l, train_loader, val_loader, model # Free up GPU memory
+        del X_test_l, model
+        
+        gc.collect()
 
     # Save data
     df_results = pd.DataFrame(all_layer_data)
