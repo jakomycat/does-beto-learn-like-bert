1993366893795ec6a4a4c3e89415379623af26a0
jakomycat <jchalarcav@gmail.com>
2026-06-01 20:04:10 -0500

    Three functions for adding roles: right-to-left, left-to-right, and bag-of-words.

---
+++ experiments/exp4_compositional/role_schemes.py
@@ -0,0 +1,20 @@
+# Function to add left-to-right role
+def left_to_right(words):
+    num_words = len(words)
+    roles = list(range(num_words))
+    
+    return roles
+
+# Function to add right-to-left role
+def right_to_left(words):
+    num_words = len(words)
+    roles = list(range(num_words - 1, -1, -1))
+    
+    return roles
+
+# Function to add bag-of-words role
+def bag_of_words(words):
+    num_words = len(words)
+    roles = [0] * num_words
+    
+    return roles
