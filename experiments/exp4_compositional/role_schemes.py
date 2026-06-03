5d12db63e97a66c7fec0f5f35455a0f82200947a
jakomycat <jchalarcav@gmail.com>
2026-06-02 23:46:34 -0500

    Function added to implement random-tree role.

--- experiments/exp4_compositional/role_schemes.py
+++ experiments/exp4_compositional/role_schemes.py
@@ -1,3 +1,5 @@
+import random
+
 # Function to add left-to-right role
 def left_to_right(words):
     num_words = len(words)
@@ -25,5 +27,37 @@ def bidirectional(words):
     right_left_role = right_to_left(words)
     
     roles = list(zip(left_right_role, right_left_role))
+    
+    return roles
+
+# Function to add random-tree role
+def random_tree_roles(words, seed=123):
+    num_words = len(words)
+    
+    # Recursion
+    if num_words == 0:
+        return []
+    if num_words == 1:
+        return [''] 
+    
+    roles = ['']*num_words
+    
+    rng = random.Random(seed)
+    
+    # Auxiliar function to recursion
+    def _build_tree(start, end, current_path):
+        # Base case
+        if start == end:
+            roles[start] = current_path
+            return
+        
+        # Recursive case
+        split =  rng.randint(start, end - 1)
+        
+        _build_tree(start, split, current_path + 'L')
+        _build_tree(split + 1, end, current_path + 'R')
+        
+    # It start the recursive function
+    _build_tree(0, num_words - 1, '')
     
     return roles
