0ea61d8279d0264905a65ddb9c2904d8cace0f3d
jakomycat <jchalarcav@gmail.com>
2026-06-04 22:52:01 -0500

    Function added to train TPDN.

--- experiments/exp4_compositional/tpdn.py
+++ experiments/exp4_compositional/tpdn.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from torch.optim import Adam
+from tqdm import tqdm
 
 class TPDN(nn.Module):
     def __init__(self, n_roles, role_dim, filler_dim, output_dim):
@@ -20,3 +22,36 @@ class TPDN(nn.Module):
         output = self.projection(tpr_flat)
         
         return output
+
+# Function to train TPDN
+def train_tpdn(tpdn, dataloader, device, n_epochs=10, lr=1e-3):
+    # Config
+    tpdn = tpdn.to(device) 
+    tpdn.train()
+    
+    optimizer = Adam(tpdn.parameters(), lr=lr)
+    criterion = nn.MSELoss()
+    
+    for epoch in range(n_epochs):
+        total_loss = 0.0
+        
+        batch_iterator = tqdm(dataloader, desc=f'Epoch {epoch + 1} / {n_epochs}')
+        for batch in batch_iterator:
+            fillers = batch['fillers'].to(device, dtype=torch.float32)
+            role_ids = batch['role_ids'].to(device, dtype=torch.long)
+            attention_mask = batch['attention_mask'].to(device, dtype=torch.float32)
+            targets = batch['targets'].to(device, dtype=torch.float32)
+            
+            optimizer.zero_grad()
+            
+            preds = tpdn(fillers, role_ids, attention_mask)
+            loss = criterion(preds, targets)
+            
+            loss.backward()
+            optimizer.step()
+            
+            total_loss += loss.item()
+            
+            batch_iterator.set_postfix({'loss' : f'{loss.item():.4f}'})
+        
+    return tpdn
