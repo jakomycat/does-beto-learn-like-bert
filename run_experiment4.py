6544ffd71ae65abb475ec2371a78db267d3e159c
jakomycat <jchalarcav@gmail.com>
2026-06-19 13:00:04 -0500

    Changes to the runner to: - Reduce the depth of the role tree. - Make minor adjustments to ensure proper functionality.

--- run_experiment4.py
+++ run_experiment4.py
@@ -21,22 +21,28 @@ def main():
         default='en'
     )
     
-    parser.add_argument( # For quick debugging
+    parser.add_argument(
         '--max_samples',
         type=int,
-        default=None
+        default=10000
+    )
+    
+    parser.add_argument(
+        '--max_depth',
+        type=int,
+        default=4
     )
     
     args = parser.parse_args()
-    lang = args.lang
+    lang, max_samples, max_depth = args.lang, args.max_samples, args.max_depth
     
     # Load model and tokenizer
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model, tokenizer = load_model_and_tokenizer(lang=lang, device=device)
     
     # Get data
-    sentences_train_raw = load_premises(split='train', max_samples=args.max_samples)
-    sentences_test_raw  = load_premises(split='test', max_samples=args.max_samples)
+    sentences_train_raw = load_premises(split='train', max_samples=max_samples)
+    sentences_test_raw  = load_premises(split='test', max_samples=max_samples)
 
     sentences_train = [s.split() for s in sentences_train_raw]
     sentences_test  = [s.split() for s in sentences_test_raw]
@@ -45,8 +51,8 @@ def main():
     nlp = stanza.Pipeline(lang=lang, processors='tokenize,pos,constituency', tokenize_pretokenized=True)
     
     # Pre-parse syntax trees
-    train_tree_roles = preparse_tree_roles(sentences_train, nlp)
-    test_tree_roles  = preparse_tree_roles(sentences_test, nlp)
+    train_tree_roles = preparse_tree_roles(sentences_train, nlp, max_depth=max_depth)
+    test_tree_roles  = preparse_tree_roles(sentences_test, nlp, max_depth=max_depth)
     
     # Build combined tree roles dictionary
     tree_roles_dict = {tuple(w): r for w, r in zip(sentences_train, train_tree_roles)}
@@ -56,8 +62,8 @@ def main():
     fillers_train = get_filler_embeddings(sentences_train, model, tokenizer, device)
     fillers_test  = get_filler_embeddings(sentences_test, model, tokenizer, device)
     
-    targets_train = get_cls_token(sentences_train, model, tokenizer, device, task_name='tpdn', split='train', is_split_into_words=True)
-    targets_test  = get_cls_token(sentences_test, model, tokenizer, device, task_name='tpdn', split='test', is_split_into_words=True)
+    targets_train = get_cls_token(sentences_train, model, tokenizer, device, task_name='tpdn', split='train', lang=lang, is_split_into_words=True)
+    targets_test  = get_cls_token(sentences_test, model, tokenizer, device, task_name='tpdn', split='test', lang=lang, is_split_into_words=True)
     
     # Define role schemes
     role_schemes_dict = {
@@ -81,7 +87,8 @@ def main():
         build_vocab_fn=build_role_vocab,
         map_roles_fn=map_roles_to_ids,
         device=device,
-        output_filename=f'tpdn_table4_{lang}'
+        output_filename=f'tpdn_table4_{lang}',
+        seeds=[42, 43, 44, 45, 46]
     )
 
 if __name__ == '__main__':
